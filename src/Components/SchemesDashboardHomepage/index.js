24263c5aefccc8f89a709b5916c703ea66613baf
Shivam Sharma <mail2shivam94@gmail.com>
2021-01-08 16:23:06 +0530

    Created scroll controls for radio buttons container

--- src/Components/SchemesDashboardHomepage/index.js
+++ src/Components/SchemesDashboardHomepage/index.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import SchemesCard from "../SchemesCard";
 
-import "./index.css"
+import RightCaret from "../../Images/arrow/right.svg"
+import LeftCaret from "../../Images/arrow/left.svg"
+
+import "./index.css";
 
 const schemes = [
   { title: "National Health Mission", link: "", class: "mt-4", img: "" },
@@ -24,21 +27,97 @@ const schemes = [
   { title: "National Health Mission", link: "", class: "mt-4", img: "" },
 ];
 
+const radioButtons = [
+  { title: "All", val: "all"},
+  { title: "Agriculture and Allied Activities ", val: "Agriculture and Allied Activities" },
+  { title: "Drinking Water & Sanitation", val: "Drinking Water & Sanitation" },
+  { title: "Education", val: "Education" },
+  { title: "Environment and Forests", val: "Environment and Forests" },
+  { title: "Food, Civil Supplies and Co-operation", val: "Food, Civil Supplies and Co-operation" },
+  { title: "Drinking Water & Sanitation", val: "Drinking Water & Sanitation" },
+  { title: "Education", val: "Education" },
+  { title: "Environment and Forests", val: "Environment and Forests" },
+  { title: "Food, Civil Supplies and Co-operation", val: "Food, Civil Supplies and Co-operation" },
+];
+
 const SchemesDashboardHomepage = () => {
+  const [schemeType, setSchemeType] = useState("all")
+  const [showLeftScrollButton, setShowLeftScrollButton] = useState(false)
+  const [showRightScrollButton, setShowRightScrollButton] = useState(true)
+  const toolbarScrollRef = useRef(null)
+  const handleChangeSchemesType = (e) => {
+    setSchemeType(e.target.value)
+  }
+
+  const handleScrollOnClick = (dir) => {
+    if(dir === 'left'){
+      toolbarScrollRef.current.scrollLeft -= 200;
+    }
+    else{
+      toolbarScrollRef.current.scrollLeft += 200;
+    }
+  }
+
+  const handleFilterOptionScroll = (e) => {
+    if(e.target.scrollLeft < 30){
+      setShowLeftScrollButton(false)
+      setShowRightScrollButton(true)
+    }
+    else{
+      if((e.target.scrollLeft + e.target.clientWidth) > e.target.scrollWidth - 30 ){
+        setShowRightScrollButton(false)
+      }
+      else{
+        setShowRightScrollButton(true)
+      }
+      setShowLeftScrollButton(true)
+    }
+
+  }
   return (
     <div className="home-layout-wrapper mt-5">
       <h1 className="page-heading text-dark pl-3 mb-2">Schemes</h1>
-      <div className="horizontal-seperator mt-3"></div>
-      <div className="radio-buttons-container"></div>
+      <div className="horizontal-seperator mt-3 mb-1"></div>
+      <div className="radio-toolbar-container mt-3">
+        {
+          showLeftScrollButton
+          ?
+          <button className="scroll-button left" onClick={() => handleScrollOnClick('left')}><img src={RightCaret} /></button>
+          : null
+        }
+        <div class="radio-toolbar d-flex tab-horizontal-scroll" onScroll={handleFilterOptionScroll} ref={toolbarScrollRef}>
+          {radioButtons.map((radio) => (
+            <>
+              <input
+                type="radio"
+                id={radio.val}
+                name="radios"
+                value={radio.val}
+                onChange={handleChangeSchemesType}
+                checked={radio.val === schemeType}
+              />
+              <label className={radio.class} htmlFor={radio.val}>
+                {radio.title}
+              </label>
+            </>
+          ))}
+        </div>
+        {
+          showRightScrollButton 
+          ?
+          <button className="scroll-button right" onClick={() => handleScrollOnClick('right')}><img src={RightCaret} /></button>
+          : null
+        }
+      </div>
       <div className="schemes-list-container">
         {/* <div className="row"> */}
         {schemes.map((scheme, index) => {
-            return (
-              // <div className="col-md-3 mr-3">
-                <SchemesCard scheme={scheme} key={index} />
-              // </div>
-            );
-          })}
+          return (
+            // <div className="col-md-3 mr-3">
+            <SchemesCard scheme={scheme} key={index} />
+            // </div>
+          );
+        })}
         {/* </div> */}
       </div>
     </div>
