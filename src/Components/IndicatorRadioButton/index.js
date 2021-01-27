e90287c6e79e27935eb2e11ce47b1a2ffdea3517
Shivam Sharma <mail2shivam94@gmail.com>
2021-01-27 16:42:01 +0530

    Added color and link on indicators

--- src/Components/IndicatorRadioButton/index.js
+++ src/Components/IndicatorRadioButton/index.js
@@ -1,27 +1,41 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import "./index.css";
 
 const IndicatorRadioButton = (props) => {
   return (
     <div className="mt-3 mb-1">
-      <div className="">
-        <input
-          type="radio"
-          id={props.indicatorName}
-          name="indicator-group"
-          className="indicator-radio-button-input"
-          value={props.indicatorName}
-          checked={props.checked}
-          onClick={props.handleIndicatorChange}
-        />
-        <label htmlFor={props.indicatorName}>{props.indicatorData[props.indicatorName].name}</label>
-      </div>
-      {props.checked ? (
-        <p className="radio-button-text">
-          {props.indicatorData[props.indicatorName].description}
-        </p>
-      ) : null}
+      
+        <div className="">
+        <Link
+        to={`${
+          "/scheme/" +
+          props.schemeSlug +
+          "/" +
+          props.indicatorData[props.indicatorName].slug
+          
+        }`}
+        onClick={() => props.handleIndicatorChange(props.indicatorName)}
+      >
+          <input
+            type="radio"
+            id={props.indicatorName}
+            name="indicator-group"
+            className="indicator-radio-button-input"
+            value={props.indicatorName}
+            checked={props.checked}
+          />
+          <label htmlFor={props.indicatorName}>
+            {props.indicatorData[props.indicatorName].name}
+          </label>
+          </Link>
+        </div>
+        {props.checked ? (
+          <p className="radio-button-text">
+            {props.indicatorData[props.indicatorName].description}
+          </p>
+        ) : null}
     </div>
   );
 };
