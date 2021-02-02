e7a01af3c6c5c6183d64e8b508ada4aed7df241f
Shivam Sharma <mail2shivam94@gmail.com>
2021-02-03 01:35:28 +0530

    Added latest topojson file, schemesNews File, updated data file

---
+++ src/Data/schemeNews.js
@@ -0,0 +1,129 @@
+export let recentDevelopmentsData = {
+  scheme_01: {
+    metadata: {
+      news: [
+        {
+          title:
+            "Pradhan Mantri Awas Yojana: PM Modi Releases Funds Worth Rs 2,691 Crore For 6.1 Lakh Beneficiaries",
+          text:
+            "Prime Minister Narendra Modi on Wednesday released financial assistance of around Rs 2,691 crores to the 6 lakh beneficiaries of Pradhan Mantri Aawas Yojna – Grameen in Uttar Pradesh.",
+          img: "",
+          class: "",
+          link:
+            "https://www.outlookindia.com/website/story/india-news-pradhan-mantri-awas-yojana-pm-modi-releases-funds-worth-rs-2691-crore-for-61-lakh-beneficiaries/371217",
+        },
+        {
+          title:
+            "Centre raises concern over slow pace of housing scheme for poor",
+          text:
+            "The Union rural development ministry has raised concerns over the “drastic slowdown” of flagship housing scheme for the rural poor and has recommended that an “intense follow-up” is required by states/Union Territories (UTs)",
+          img: "",
+          class: "ml-4",
+          link:
+            "https://www.hindustantimes.com/india-news/centre-raises-concerns-over-slow-pace-of-housing-scheme-for-poor/story-FSUp4m2TRFLZbIZoX2LKpJ.html",
+        },
+      ],
+    },
+  },
+  scheme_02: {
+    metadata: {
+      news: [
+        {
+          title:
+            "Vice President suggests inclusion of milk in mid-day meal scheme",
+          text:
+            "The Vice President, Shri M Venkaiah Naidu has suggested that milk could be given either as part of breakfast or the mid-day meal in order to improve the nutritional levels of children.",
+          img: "",
+          class: "",
+          link: "https://pib.gov.in/PressReleasePage.aspx?PRID=1651938",
+        },
+        {
+          title: "KU releases social audit report of Mid-Day Meal Scheme",
+          text:
+            "The University of Kashmir on Monday released the findings of a social audit conducted into the Mid-Day Meal Scheme in select schools of Kashmir division.",
+          img: "",
+          class: "ml-4",
+          link:
+            "https://thekashmirimages.com/2020/12/29/ku-releases-social-audit-report-of-mid-day-meal-scheme/",
+        }
+      ],
+    },
+  },
+  scheme_03: {
+    metadata: {
+      news: [
+        {
+          title:
+            "Last-mile hurdles in NREGA payments puncture India’s techno-utopian delusions",
+          text:
+            "A latest report by LibTech India, Length of the Last Mile, paints a grim and startling picture of these last-mile hurdles, based on a careful survey of 1,947 NREGA workers in Andhra Pradesh, Jharkhand and Rajasthan.",
+          img: "",
+          class: "",
+          link:
+            "https://scroll.in/article/978862/jean-dreze-last-mile-hurdles-in-nrega-payments-puncture-indias-techno-utopian-delusions",
+        },
+        {
+          title:
+            "Centre left with mere 10% NREGA funds as scheme sees 243% jump in Corona times",
+          text:
+            "In the time of Covid pandemic, the Central government has made a record payment to MGNREGA workers in the financial year 2020-21. Though there was a surge for NREGA work demand in 2020, a report (by People Action for Employment Guarantee (PAEG), a network of NGOs) states that the average work provided to individual households was less than the previous years.",
+          img: "",
+          class: "ml-4",
+          link:
+            "https://www.hindustantimes.com/india-news/center-left-with-mere-10-nrega-funds-as-scheme-sees-243-jump-in-corona-times/story-vyKbeho4tgGUCsJBf6XFaK.html",
+        },
+      ],
+    },
+  },
+  scheme_04: {
+    metadata: {
+      news: [
+        {
+          title:
+            "States that spend more on healthcare less affected by Covid impact",
+          text:
+            "India’s public healthcare expenditure is ₹1,765 per capita, which is 2.5 times less than the cost of a single Covid-19 test which the government has notified. However, there are a number of States which spend substantially more than the national average.",
+          img: "",
+          class: "",
+          link:
+            "https://www.thehindubusinessline.com/opinion/states-that-spend-more-on-healthcare-less-affected-by-covid/article31738995.ece",
+        },
+        {
+          title: "India’s health budget fourth lowest in world: Oxfam",
+          text:
+            "Latest “Inequality Index 2020” by Oxfam states that India and Afghanistan set aside the same share for health in their budget i.e. 4 per cent which is just above a quarter of the recommended spending.",
+          img: "",
+          class: "ml-4",
+          link:
+            "https://timesofindia.indiatimes.com/india/indias-health-budget-fourth-lowest-in-world-oxfam/articleshow/78597933.cms",
+        },
+      ],
+    },
+  },
+  scheme_05: {
+    metadata: {
+      news: [
+        {
+          title:
+            "Maharashtra government seeks central funds for ICDS programme",
+          text:
+            "Women and Child Development (WCD) minster of Maharashtra wrote to Union minister Smriti Irani asking for additional fund to the tune of Rs. 2,000 crores to implement ICDS scheme as the number of beneficiaries has increased during the pandemic.",
+          img: "",
+          class: "",
+          link:
+            "https://timesofindia.indiatimes.com/city/pune/state-government-seeks-central-funds-for-icds-programme/articleshow/79818237.cms",
+        },
+        {
+          title:
+            "Training to new mothers at government hospitals in Tiruchi through ICDS",
+          text:
+            "A new project under Integrated Child Development Services (ICSD), inaugurated on Friday, aims to give guidance to new mothers on how to care for the infant at home after discharge from the hospital. Under the initiative, christened 'Chellapillai,' counselling would be given to at least 50 to 60 mothers in a day.",
+          img: "",
+          class: "ml-4",
+          link:
+            "https://www.thehindu.com/news/cities/Tiruchirapalli/training-to-new-mothers-at-gh-through-icds/article33482060.ece",
+        },
+      ],
+    },
+  },
+};
