265d72b2bb8f9b684930cd91d99c7cccdabd7bec
Shivam Sharma <mail2shivam94@gmail.com>
2021-02-03 20:39:16 +0530

    Added logo files for schemes

---
+++ src/Data/schemesLogos.js
@@ -0,0 +1,14 @@
+import pmay from "../Images/schemesLogos/pmay.svg"
+import icds from "../Images/schemesLogos/icds.png"
+import mdm from "../Images/schemesLogos/mdm.svg"
+import mgnrega from "../Images/schemesLogos/mgnrega.png"
+import nhm from "../Images/schemesLogos/nhm.png"
+
+const schemeLogos = {
+    scheme_01 : pmay,
+    scheme_02 : icds,
+    scheme_03 : mdm,
+    scheme_04 : mgnrega,
+    scheme_05 : nhm
+} 
+export default schemeLogos;
