369466dbebbdcb02481c6becfad808886056bd50
Shivam Sharma <mail2shivam94@gmail.com>
2021-02-10 15:46:37 +0530

    Updated Recent Developments data

--- src/Data/schemeNews.js
+++ src/Data/schemeNews.js
@@ -86,6 +86,124 @@ export let recentDevelopmentsData = {
       news: [
         {
           title:
+            "15th Finance Commission recommends sectoral grant for states formaintaining roads under PMGSY",
+          text:
+            "The 15th Finance Commission has recommended a grant of Rs. 27,539 crore for states, for maintenance of roads under PMGSY. The grant is for the period 2021-26. Of the total grant, Rs. 12,796 crore is allotted to North Eastern Hill (NEH) states.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://fincomindia.nic.in/writereaddata/html_en_files/fincom15/Reports/XVFC%20Complete_Report.pdf",
+        },
+        {
+          title:
+            "1858 out of 2149 eligible habitations in Jammu and Kashmir have been connected by road under PMGSY",
+          text:
+            "Following resolution of pending road works, progress has been made under PMGSY in Jammu and Kashmir, and Ladakh. Many of the sanctioned roads and bridges have been constructed, with an expenditure of more than Rs. 715 crore in 2020.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://pib.gov.in/PressReleasePage.aspx?PRID=1646462",
+        },
+        {
+          title:
+            "Rs. 15,000 crore allocated for PMGSY in 2021-22, decrease from previous year",
+          text:
+            "An allocation of Rs. 15,000 crore was made for PMGSY in Union Budget 2021-22, which marks a 23% reduction from the previous year’s allocation.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.moneycontrol.com/news/business/budget-2021-fm-nirmala-sitharaman-allocates-rs-15000-crore-for-rural-roads-scheme-pmgsy-6427111.html",
+        },
+        {
+          title:
+            "Centre reduces PMGSY target for Odisha following request by state",
+          text:
+            "The Ministry of Rural Development has reduced the PMGSY target for Odisha by 35 km. Odisha had requested for a greater reduction (from 3,720 km to 2,095 km) in light of the disruptions caused by the pandemic.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://www.newindianexpress.com/states/odisha/2021/jan/20/centre-reduces-pmgsy-target-for-odisha-by-35-km-2252601.html",
+        },
+        {
+          title:
+            "Third phase of PMGSY launched with target of 1.25 lakh km road connectivity",
+          text:
+            "With two phases completed, the third phase of PMGSY has been launched with a target of 1.25 lakh km all-weather road connectivity. 6.44 lakh km road length has already been constructed under the scheme.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.indiabudget.gov.in/economicsurvey/doc/vol2chapter/echap10_vol2.pdf",
+        }
+      ],
+    },
+  },
+  scheme_05: {
+    metadata: {
+      news: [
+        {
+          title:
+            "Funds allotted to states/UTs under Samagra Shiksha Abhiyan to promote digital learning",
+          text:
+            "To mitigate the impact of COVID-19, Rs. 818.17 crore has been allotted to states/UTs to promote online learning through digital initiatives, and Rs. 267.86 crore for online teacher training to ensure continuous professional development of teachers under SMSA.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.indiabudget.gov.in/economicsurvey/doc/vol2chapter/echap10_vol2.pdf",
+        },
+        {
+          title: "Lower allocation for SMSA in Union Budget 2021-22 may affect progress towards Right to Education",
+          text:
+            "The budget for SMSA in 2021-22 has reduced to Rs.31,050 crore, from Rs. 36,400 crore in 2020-21. This is a concern given the infrastructural deficits and teacher shortages in government schools, and is likely to hurt marginalised children who are at greater risk of dropping out following the COVID crisis.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://scroll.in/article/985788/budget-2021-endangers-decades-of-work-in-the-right-to-education-movement",
+        },
+        {
+          title: "Tamil Nadu raises salary for part-time teachers under SMSA",
+          text:
+            "The Tamil Nadu government has increased the monthly salaries of part-time teachers under Samagra Shiksha Abhiyan from Rs. 7770 to Rs. 10,000.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://timesofindia.indiatimes.com/city/chennai/tamil-nadu-raises-salary-for-part-time-school-teachers/articleshow/80645744.cms",
+        },
+        {
+          title: "STARS project for school education to be implemented with financial support from World Bank",
+          text:
+            "The STARS project will cost Rs. 5718 crore, of which Rs. 3700 crore will be funded by World Bank. The project will support interventions to improve school education quality in 6 states, and will draw on the existing SMSA structure.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://pib.gov.in/PressReleasePage.aspx?PRID=1693142",
+        },
+        {
+          title: "Delhi Government launches survey under SMSA to identify out-of-school children",
+          text:
+            "The education department of the Delhi Government will conduct a survey to identify out-of-school children in the ages of 6-14 and enrol them in schools.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.newindianexpress.com/cities/delhi/2021/jan/08/235-teams-to-conduct-survey-to-identify-out-of-school-children-in-delhi-2247169.html ",
+        },
+      ],
+    },
+  },
+  scheme_06: {
+    metadata: {
+      news: [
+        {
+          title:
             "Last-mile hurdles in NREGA payments puncture India’s techno-utopian delusions",
           text:
             "A latest report by LibTech India, Length of the Last Mile, paints a grim and startling picture of these last-mile hurdles, based on a careful survey of 1,947 NREGA workers in Andhra Pradesh, Jharkhand and Rajasthan.",
@@ -109,7 +227,7 @@ export let recentDevelopmentsData = {
       ],
     },
   },
-  scheme_05: {
+  scheme_07: {
     metadata: {
       news: [
         {
@@ -133,6 +251,119 @@ export let recentDevelopmentsData = {
           link:
             "https://timesofindia.indiatimes.com/india/indias-health-budget-fourth-lowest-in-world-oxfam/articleshow/78597933.cms",
         },
+      ],
+    },
+  },
+  scheme_08: {
+    metadata: {
+      news: [
+        {
+          title:
+            "For 2021-25, HRD Ministry seeks ₹10.37 trillion from 15th Finance Commission",
+          text:
+            "The Human Resource Development Ministry on Monday said it needs Rs.10.37 trillion for both for the higher education and school education sectors and gave a detailed requirement projection to the 15th Finance Commission which is headed by N.K. Singh.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.livemint.com/news/india/for-2021-25-hrd-ministry-seeks-rs-10-37-trillion-from-15th-finance-commission-11593451655773.html",
+        },
+        {
+          title: "Student-teacher ratio high at secondary level: Report",
+          text:
+            "According to a meeting of the Project Approval Board (PAB), the required student-teacher ratio in government secondary schools, according to the Rashtriya Madhyamik Shiksha Abhiyan (RMSA) framework, should be 30:1. But it is 51:1 for languages, 102:1 for mathematics, 165:1 for social studies and 237:1 for science in Haryana.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://www.tribuneindia.com/news/haryana/student-teacher-ratio-high-at-secondary-level-report-129970",
+        },
+        {
+          title: "RMSA teachers appeal to release salary",
+          text:
+            "Teachers under RMSA have not been paid their salaries for last five months. The teachers’ association has demanded the state government to release the salaries immediately.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://easternmirrornagaland.com/nagaland-rmsa-teachers-appeal-to-release-salary/",
+        },
+        {
+          title: "Special teams to inspect Tiruchy schools before resuming classes on Tuesday",
+          text:
+            "In order to get schools ready for reopening after the Covid induce lockdown, available funds under RMSA will be used for things like scanner, and hand-sanitiser.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://www.edexlive.com/news/2021/jan/18/special-teams-under-to-inspect-tiruchy-schools-before-resuming-classes-on-tuesday-17343.html",
+        },
+        {
+          title: "Technical issues delay disbursal of salary to aided HSS teachers",
+          text:
+            "Around 11,000 teachers in Kerala are facing the prospect of delayed salary for the month of January due to some technical difficulties in cross verification of records.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.thehindu.com/news/national/kerala/technical-issues-delay-disbursal-of-salary-to-aided-hss-teachers/article33786872.ece",
+        }
+      ],
+    },
+  },
+  scheme_09: {
+    metadata: {
+      news: [
+        {
+          title: "Covid-19 impact: Finance Commission discusses new tools for education",
+          text:
+            "The Finance Commission conducted a meeting with HRD minister and discussed the impact of the new tools of education including online classes and use of other technology amid the coronavirus pandemic.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.business-standard.com/article/education/covid-19-impact-finance-commission-discusses-new-tools-for-education-120063000086_1.html",
+        },
+        {
+          title: "Over 35000 specially-abled students enrolled in Government schools in 2 years",
+          text:
+            "A total of 35,569 specially-abled students have been enrolled in the government schools in the elementary and secondary-level classes under the Inclusive Education (IE) programme, which was launched in 2018 as one of the major components of the erstwhile Sarva Shiksha Abhiyan (SSA) and Rashtriya Madhyamik Shiksha Abhiyan (RMSA) schemes.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://www.greaterkashmir.com/news/kashmir/over-35000-specially-abled-students-enrolled-in-government-schools-in-2-years/",
+        },
+        {
+          title: "Odisha to shut over 8,000 primary schools with less than 20 students",
+          text:
+            "The Odisha government has decided to shut 8,000-odd primary and upper primary state schools from the current academic year on the ground that their minimal student strength is not viable to let them run.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.hindustantimes.com/india-news/odisha-to-shut-over-8-000-primary-schools-with-less-than-20-students/story-kFYeSA4pTyyUd9TGhleFBJ.html",
+        },
+        {
+          title: "India's Small Schools are Failing to Provide Equal Education to Rural Children",
+          text:
+            "The Annual Status of Education Report (ASER) 2014 brought out that the learning outcomes measured in terms of reading and arithmetic have been declining across the country from 2006 to 2014. This result is surprising in the wake of implementation of Sarva Shiksha Abhiyan (SSA) from 2000.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "ml-4",
+          link:
+            "https://www.news18.com/news/opinion/indias-small-schools-are-failing-to-provide-equal-education-to-rural-children-2935153.html",
+        },
+        {
+          title: "Sisodia requests Union education minister to extend contracts of SSA teachers",
+          text:
+            "Delhi education minister Manish Sisodia on Thursday wrote to Union education minister Ramesh Pokhriyal ‘Nishank’, requesting him to extend the contracts of 2,766 teachers working in the national capital under the Centre’s Samagra Shiksha Abhiyan (SSA) till March 31, 2021.",
+          img: "",
+          accessed_on: "09 February 2021",
+          class: "",
+          link:
+            "https://www.hindustantimes.com/cities/delhi-news/sisodia-requests-union-education-minister-to-extend-contracts-of-ssa-teachers-101611256948573.html",
+        }
       ],
     },
   },
