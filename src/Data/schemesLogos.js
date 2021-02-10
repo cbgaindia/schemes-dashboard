5c554d3156521f2246a9f597bbad5ee41cf0ddfd
Shivam Sharma <mail2shivam94@gmail.com>
2021-02-10 15:47:10 +0530

    Added logos for new schemes

--- src/Data/schemesLogos.js
+++ src/Data/schemesLogos.js
@@ -3,12 +3,19 @@ import icds from "../Images/schemesLogos/icds.png"
 import mdm from "../Images/schemesLogos/mdm.png"
 import mgnrega from "../Images/schemesLogos/mgnrega.png"
 import nhm from "../Images/schemesLogos/nhm.png"
+import pmgsy from "../Images/schemesLogos/pmgsy.jpg"
+import rmsa from "../Images/schemesLogos/rmsa.jpeg"
+import ssa from "../Images/schemesLogos/ssa.jpg"
 
 const schemeLogos = {
     scheme_01 : pmay,
     scheme_02 : icds,
     scheme_03 : mdm,
-    scheme_04 : mgnrega,
-    scheme_05 : nhm
+    scheme_04 : pmgsy,
+    scheme_05 : ssa,
+    scheme_06 : mgnrega,
+    scheme_07 : nhm,
+    scheme_08 : rmsa,
+    scheme_09 : ssa,
 } 
 export default schemeLogos;
