11ee6d6c1b48f48d0a51b9010683b622236b79df
Shivam Sharma <mail2shivam94@gmail.com>
2021-02-11 15:38:31 +0530

    Updated SMSA logo

--- src/Data/schemesLogos.js
+++ src/Data/schemesLogos.js
@@ -6,13 +6,14 @@ import nhm from "../Images/schemesLogos/nhm.png"
 import pmgsy from "../Images/schemesLogos/pmgsy.jpg"
 import rmsa from "../Images/schemesLogos/rmsa.jpeg"
 import ssa from "../Images/schemesLogos/ssa.jpg"
+import smsa from "../Images/schemesLogos/smsa.jpg"
 
 const schemeLogos = {
     scheme_01 : pmay,
     scheme_02 : icds,
     scheme_03 : mdm,
     scheme_04 : pmgsy,
-    scheme_05 : ssa,
+    scheme_05 : smsa,
     scheme_06 : mgnrega,
     scheme_07 : nhm,
     scheme_08 : rmsa,
