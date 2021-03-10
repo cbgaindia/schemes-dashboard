144c5b16b41c721610c3c8cdfc6c4d9320c62a65
Shivam Sharma <mail2shivam94@gmail.com>
2021-03-10 14:21:11 +0530

    Updated new scheme logos

--- src/Data/schemesLogos.js
+++ src/Data/schemesLogos.js
@@ -7,16 +7,36 @@ import pmgsy from "../Images/schemesLogos/pmgsy.jpg"
 import rmsa from "../Images/schemesLogos/rmsa.jpeg"
 import ssa from "../Images/schemesLogos/ssa.jpg"
 import smsa from "../Images/schemesLogos/smsa.jpg"
+import bbbp from "../Images/schemesLogos/bbbp.png"
+import br from "../Images/schemesLogos/br.jpg"
+import icps from "../Images/schemesLogos/icps.png"
+import jjm from "../Images/schemesLogos/jjm.jpg"
+import osc from "../Images/schemesLogos/osc.jpg"
+import pa from "../Images/schemesLogos/pa.png"
+import rkvy from "../Images/schemesLogos/rkvy.png"
+import sbmu from "../Images/schemesLogos/sbmu.png"
+import sg from "../Images/schemesLogos/sg.jpeg"
+
 
 const schemeLogos = {
-    scheme_01 : pmay,
+    scheme_01 : jjm,
     scheme_02 : icds,
-    scheme_03 : mdm,
+    scheme_03 : sg,
     scheme_04 : pmgsy,
-    scheme_05 : smsa,
-    scheme_06 : mgnrega,
-    scheme_07 : nhm,
-    scheme_08 : rmsa,
-    scheme_09 : ssa,
+    scheme_05 : mgnrega,
+    scheme_06 : smsa,
+    scheme_07 : bbbp,
+    scheme_08 : mdm,
+    scheme_09 : pmay,
+    scheme_10 : rmsa,
+    scheme_11 : sbmu,
+    scheme_12 : rkvy,
+    scheme_13 : br,
+    scheme_14 : osc,
+    scheme_15 : pa,
+    scheme_16 : ssa,
+    scheme_17 : sbmu,
+    scheme_18 : icps,
+    scheme_19 : nhm,
 } 
 export default schemeLogos;
