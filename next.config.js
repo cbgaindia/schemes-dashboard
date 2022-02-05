94cc17852e4111dedfc0b78c63b0fcc0528f3146
Abhinav Kumar <a.kumar2102@gmail.com>
2022-02-06 02:12:01 +0530

    FIXED - config error

--- next.config.js
+++ next.config.js
@@ -54,17 +54,3 @@ module.exports = {
   },
 };
 
-
-const withTM = require("next-transpile-modules")(["echarts", "zrender"]);
-// const withTM = require('next-transpile-modules')(['d3-fetch']);
-
-module.exports = withTM({
-  i18n: {
-    locales: ["en", "fr", "nl-NL", "te"],
-    defaultLocale: "en",
-  },
-  publicRuntimeConfig: {
-    DMS: "https://openbudgetsindia.org",
-    CMS: "https://oddk.home.blog",
-  },
-});
